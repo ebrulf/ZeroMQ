3406f363b7e14ddc0da13307db0bedbc8fb19d6f
Konstanty Dąbrowski <85363333+ebrulf@users.noreply.github.com>
2024-07-24 22:31:02 +0200

    repurpouse ciąg dalszy

--- GUI/OApce.xaml.cs
+++ GUI/OApce.xaml.cs
@@ -7,9 +7,26 @@ public partial class OApce : ContentPage
 	{
 		InitializeComponent();
 	}
-    private async void LearnMore_Clicked(object sender, EventArgs e)
+    private void LearnMore_Clicked(object sender, EventArgs e)
     {
         // Navigate to the specified URL in the system browser.
-        await Launcher.Default.OpenAsync("https://aka.ms/maui");
+        Console.Beep(440, 500);
+        //await Launcher.Default.OpenAsync("https://aka.ms/maui");
+    }
+    void OnColorsRadioButtonCheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        //((RadioButton)sender) //ma wyboldowaæ tekst, ale to da siê zrobiæ w xaml
+        IP.IsEnabled = (Serwer.IsChecked==true);//o to chodzi³o!
+        //RadioButtonGroup.SelectedValueProperty.
+        //Serwer.
+    }
+    void OnEntryCompleted(object sender, EventArgs e)
+    {
+        //((RadioButton)sender) //ma wyboldowaæ tekst, ale to da siê zrobiæ w xaml
+        Console.Beep(440, 500);
+    }
+    void Add(object sender, CheckedChangedEventArgs e)
+    {
+        //narzeka, ¿e tego nie ma
     }
 }
